3777e1c6063a32f2fba24f82b60d70a8ad884b0a
cata <catamocris1@gmail.com>
2025-04-13 14:17:41 +0300

    etapa 1

---
+++ treasure_manager.c
@@ -0,0 +1,368 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+
+#define ID_LENGTH 15
+#define USER_LENGTH 15
+#define CLUE_LENGTH 100
+
+typedef struct{
+    char id[ID_LENGTH];
+    char user[USER_LENGTH];
+    float latitude;
+    float longitude;
+    char clue[CLUE_LENGTH];
+    int value;
+}Treasure_t;
+
+
+void add(char *hunt_id){
+
+    // verific daca exista directorul hunt, creez daca nu exista
+    struct stat buf;
+    if(stat(hunt_id, &buf) != 0){
+        if(mkdir(hunt_id, S_IRUSR | S_IWUSR | S_IXUSR) != 0){
+            perror("failed to create directory");
+            exit(-1);
+        }
+        printf("directory '%s' created successfully \n\n", hunt_id);
+    }
+    else{
+        printf("adding to directory '%s'\n\n", hunt_id);
+    }
+
+    // deschid directorul
+    DIR *dir = opendir(hunt_id);
+    if(dir == NULL){
+        perror("failed to open directory");
+        exit(-1);
+    }
+
+    // inchid directorul
+    if(closedir(dir) == -1){
+        perror("failed to close directory");
+        exit(-1);
+    }
+
+    // citesc datele pt treasure
+    Treasure_t treasure;
+
+    printf("Enter treasure ID: ");
+    scanf("%14s", treasure.id);
+
+    printf("Enter username: ");
+    scanf("%14s", treasure.user);
+
+    printf("Enter latitude: ");
+    scanf("%f", &treasure.latitude);
+
+    printf("Enter longitude: ");
+    scanf("%f", &treasure.longitude);
+
+    printf("Enter clue: ");
+    while(getchar() != '\n'); // ca sa nu citeasca newline
+    fgets(treasure.clue, sizeof(treasure.clue), stdin);
+    treasure.clue[strcspn(treasure.clue, "\n")] = '\0'; // strcspn cauta prima aparitie a unui caracter din al doilea string, returneaza indexul
+
+    printf("Enter value: ");
+    scanf("%d", &treasure.value);
+
+    // scriu datele in fisierul treasures
+    char treasure_filepath[100];
+    snprintf(treasure_filepath, sizeof(treasure_filepath), "%s/treasures.bin", hunt_id);
+    int treasure_fd = open(treasure_filepath, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IXUSR); 
+    if(treasure_fd == -1){
+        perror("failed to open treasures file");
+        exit(-1);
+    }
+
+    if(write(treasure_fd, &treasure, sizeof(Treasure_t)) == -1){
+        perror("failed to write treasure data");
+        close(treasure_fd);
+        exit(-1);
+    }
+
+    if(close(treasure_fd) == -1){
+        perror("failed to close file");
+        exit(-1);
+    }
+
+    // dau log la operatia facuta
+    char log_filepath[100];
+    snprintf(log_filepath, sizeof(log_filepath), "%s/logged_hunt.bin", hunt_id);
+    int log_fd = open(log_filepath, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IXUSR);
+    if(log_fd == -1){
+        perror("failed to open log file");
+        exit(-1);
+    }
+
+    char log_msg[100];
+    snprintf(log_msg, sizeof(log_msg), "Added treasure %s\n", treasure.id);
+
+    if(write(log_fd, log_msg, strlen(log_msg)) == -1){
+        perror("failed to log message");
+        close(log_fd);
+        exit(-1);
+    }
+
+    if(close(log_fd) == -1){
+        perror("failed to close file");
+        exit(-1);
+    }
+
+    // creez legatura simbolica
+    char symlink_filepath[100];
+    snprintf(symlink_filepath, sizeof(symlink_filepath), "logged_hunt_%s", hunt_id);
+    int symlink_fd = symlink(log_filepath, symlink_filepath); // daca exista deja, nu se face overwrite
+
+    printf("\nadded treasure %s to hunt %s\n", treasure.id, hunt_id);
+
+}
+
+void list(char* hunt_id){
+    
+    // fac filepath ul pt a putea deschide fisierul
+    char treasure_filepath[100];
+    snprintf(treasure_filepath, sizeof(treasure_filepath), "%s/treasures.bin", hunt_id);
+
+    // deschid fisierul
+    int treasure_fd = open(treasure_filepath, O_RDONLY);
+    if(treasure_fd == -1){
+        perror("failed to open treasures file");
+        exit(-1);
+    }
+
+    // afisez
+    printf("Hunt: %s\n", hunt_id);
+
+    Treasure_t treasure;
+    int len;
+    while(len = read(treasure_fd, &treasure, sizeof(Treasure_t))){
+        if(len < sizeof(Treasure_t)){
+            perror("error reading treasure data");
+            exit(-1);
+        }
+        printf("Treasure ID: %s -- User: %s -- Latitude: %f -- Longitude: %f -- Clue: %s -- Value: %d\n",
+                treasure.id, treasure.user, treasure.latitude, treasure.longitude, treasure.clue, treasure.value);        
+    }
+
+    // file info 
+    struct stat info;
+    if(stat(treasure_filepath, &info) == -1){
+        perror("failed to get file info");
+        close(treasure_fd);
+        exit(-1);
+    }
+
+    printf("\nFile size: %ld bytes \n", info.st_size);
+    printf("Last modified: %s \n", ctime(&info.st_mtime));
+
+    // inchid fisierul
+    if(close(treasure_fd) == -1){
+        perror("failed to close file");
+        exit(-1);
+    }
+}
+
+void view(char* hunt_id, char* treasure_id){
+    
+    char treasure_filepath[100];
+    snprintf(treasure_filepath, sizeof(treasure_filepath), "%s/treasures.bin", hunt_id);
+
+    int treasure_fd = open(treasure_filepath, O_RDONLY);
+    if(treasure_fd == -1){
+        perror("failed to open treasures file");
+        exit(-1);
+    }
+
+    Treasure_t treasure;
+    int len;
+    char treasure_msg[500];
+    
+    while((len = read(treasure_fd, &treasure, sizeof(Treasure_t))) > 0){
+
+
+        // cand gasesc id ul, afisez datele
+        if(strcmp(treasure.id, treasure_id) == 0){
+            snprintf(treasure_msg, sizeof(treasure_msg), 
+                "Treasure ID: %s -- User: %s -- Latitude: %f -- Longitude: %f -- Clue: %s -- Value: %d\n",
+                treasure.id, treasure.user, treasure.latitude, treasure.longitude, treasure.clue, treasure.value);
+            printf("%s", treasure_msg);
+            break;
+        }
+    }
+
+    if(close(treasure_fd) == -1){
+        perror("failed to close file");
+        exit(-1);
+    }
+
+}
+
+void remove_treasure(char* hunt_id, char* treasure_id){
+
+    // scriu datele de la treasures intr-un fisier auxiliar, mai putin de la treasure ul care trebuie sters
+    char treasure_filepath[100], aux_filepath[100];
+    snprintf(treasure_filepath, sizeof(treasure_filepath), "%s/treasures.bin", hunt_id);
+    snprintf(aux_filepath, sizeof(aux_filepath), "%s/aux.bin", hunt_id);
+
+    int treasure_fd = open(treasure_filepath, O_RDONLY);
+    if(treasure_fd == -1){
+        perror("failed to open treasures file");
+        exit(-1);
+    }
+
+    int aux_fd = open(aux_filepath, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR | S_IXUSR);
+    if(aux_fd == -1){
+        perror("failed to open temporary file");
+        exit(-1);
+    }
+
+    Treasure_t treasure;
+    int found = 0;
+    while(read(treasure_fd, &treasure, sizeof(Treasure_t)) == sizeof(Treasure_t)){
+        if(strcmp(treasure.id, treasure_id) == 0){
+            found = 1;
+            continue;   // daca gasim treasure ul, sarim peste
+        }
+        if(write(aux_fd, &treasure, sizeof(Treasure_t)) == -1){
+            perror("failed to write in temporary file");
+            close(treasure_fd);
+            close(aux_fd);
+            exit(-1);
+        }
+    }
+
+    close(treasure_fd);
+    close(aux_fd);
+
+    if(found == 0){
+        printf("treasure %s not found \n", treasure_id);
+        remove(aux_filepath);
+        return;
+    }
+
+    // inlocuiesc fisierul treasures original cu cel auxiliar
+    if(remove(treasure_filepath) == -1){
+        perror("failed to remove original treasures file");
+        exit(-1);
+    }
+
+    if(rename(aux_filepath, treasure_filepath) == -1){
+        perror("failed to rename temporary file");
+        exit(-1);
+    }
+
+    printf("removed treasure %s from hunt %s \n", treasure_id, hunt_id);
+
+    // log la stergerea treasure ului
+    char log_filepath[100];
+    snprintf(log_filepath, sizeof(log_filepath), "%s/logged_hunt.bin", hunt_id);
+
+    int log_fd = open(log_filepath, O_WRONLY | O_APPEND);
+    if(log_fd == -1){
+        perror("failed to open log file");
+        exit(-1);
+    }
+
+    char log_msg[100];
+    snprintf(log_msg, sizeof(log_msg), "Removed treasure %s\n", treasure_id);
+
+    if(write(log_fd, log_msg, strlen(log_msg)) == -1){
+        perror("failed to write in log file");
+        exit(-1);
+    }
+
+    if(close(log_fd) == -1){
+        perror("failed to close log file");
+        exit(-1);
+    }
+}
+
+void remove_hunt(char* hunt_id){
+
+    // merg in director si sterg toate fisierele
+    DIR* dir = opendir(hunt_id);
+    if(dir == NULL){
+        perror("failed to open directory");
+        exit(-1);
+    }
+
+    struct dirent* f;
+    char filepath[300];
+
+    while((f = readdir(dir)) != NULL){
+        // skip . (leg director curent) si .. (leg director parinte)
+        if(strcmp(f->d_name, ".") == 0 || strcmp(f->d_name, "..") == 0){
+            continue;
+        }
+
+        snprintf(filepath, sizeof(filepath), "%s/%s", hunt_id, f->d_name);
+        if(remove(filepath) == -1){
+            perror("failed to remove file in hunt");
+            closedir(dir);
+            exit(-1);
+        }
+    }
+
+    if(closedir(dir) == -1){
+        perror("failed to close directory");
+        exit(-1);
+    }
+
+    // sterg directorul
+    if(rmdir(hunt_id) == -1){
+        perror("failed to remove hunt directory");
+        exit(-1);
+    }
+    
+    // sterg legatura simbolica
+    char symlink_filepath[100];
+    snprintf(symlink_filepath, sizeof(symlink_filepath), "logged_hunt_%s", hunt_id);
+    if(remove(symlink_filepath) == -1){
+        perror("failed to remove symbolic link");
+        exit(-1);
+    }
+
+    printf("removed %s directory\n", hunt_id);
+}
+
+int main(int argc, char** argv){
+    
+    if(argc < 3){
+        perror("not enough arguments");
+        exit(-1);
+    }
+
+    if(strcmp(argv[1], "--add") == 0 && argc == 3){
+        add(argv[2]);
+        return 0;
+    }
+
+    if(strcmp(argv[1], "--list") == 0 && argc == 3){
+        list(argv[2]);
+        return 0;
+    }
+
+    if(strcmp(argv[1], "--view") == 0 && argc == 4){
+        view(argv[2], argv[3]);
+        return 0;
+    }
+
+    if(strcmp(argv[1], "--remove_treasure") == 0 && argc == 4){
+        remove_treasure(argv[2], argv[3]);
+        return 0;
+    }
+
+    if(strcmp(argv[1], "--remove_hunt") == 0 && argc == 3){
+        remove_hunt(argv[2]);
+        return 0;
+    }
+
+    return 0;
+}
